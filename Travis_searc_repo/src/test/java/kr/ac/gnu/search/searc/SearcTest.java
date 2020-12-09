910076dd3287df5bbc5177ca30b229d1dc9fe4d2
kimjangsung <wkdtjd3785@naver.com>
2020-12-09 16:35:01 +0900

    f

---
+++ Travis_searc_repo/src/test/java/kr/ac/gnu/search/searc/SearcTest.java
@@ -0,0 +1,16 @@
+package kr.ac.gnu.search.searc;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SearcTest 
+{
+	@Test
+	public void testSearch() {
+		Searc s = new Searc();
+		assertNotNull(s);
+	}
+}
